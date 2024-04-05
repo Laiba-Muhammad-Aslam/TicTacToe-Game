8659606bde4fee996ee461adf7d7254061c2052e
Laiba-Muhammad-Aslam <laiba.ky122@gmail.com>
2024-04-05 16:20:05 +0500

    Implement tic-tac-toe game logic

--- app.js
+++ app.js
@@ -5,45 +5,88 @@ let resetBtn = document.querySelector("#reset-btn");
 let boxes = document.querySelectorAll(".box");
 
 const winPatterns = [
-    [0, 1, 2],
-    [0, 3, 6],
-    [0, 4, 8],
-    [1, 4, 7],
-    [2, 5, 8],
-    [2, 4, 6],
-    [3, 4, 5],
-    [6, 7, 8],
-  ];
+  [0, 1, 2],
+  [0, 3, 6],
+  [0, 4, 8],
+  [1, 4, 7],
+  [2, 5, 8],
+  [2, 4, 6],
+  [3, 4, 5],
+  [6, 7, 8],
+];
 
-let turn0 = true;
+let turnO = true;
+let count = 0;
 
-function resetGame() {
-    turn0 = true;
-    boxes.forEach((box) => {
-        box.innerHTML = "";
-    });
-    msg.innerHTML = "";
-    msgContainer.style.display = "none";
-}
+const resetGame = () => {
+  turnO = true;
+  count = 0;
+  enableBoxes();
+  msgContainer.classList.add("hide");
+};
+
+boxes.forEach((box) => {
+    box.addEventListener("click", handleClick);
+  });
+  
+  function handleClick() {
+    if (turnO) {
+      this.innerText = "O";
+      turnO = false;
+    } else {
+      this.innerText = "X";
+      turnO = true;
+    }
+    this.disabled = true;
+    count++;
+  
+    let isWinner = checkWinner();
+  
+    if (count === 9 && !isWinner) {
+      gameDraw();
+    }
+    this.removeEventListener("click", handleClick);
+  }
+  
+const gameDraw = () => {
+  msg.innerText = `Game was a Draw.`;
+  msgContainer.classList.remove("hide");
+  disableBoxes();
+};
+
+const disableBoxes = () => {
+  for (let box of boxes) {
+    box.disabled = true;
+  }
+};
+
+const enableBoxes = () => {
+  for (let box of boxes) {
+    box.disabled = false;
+    box.innerText = "";
+  }
+};
+
+const showWinner = (winner) => {
+  msg.innerText = `Congratulations, Winner is ${winner}`;
+  msgContainer.classList.remove("hide");
+  disableBoxes();
+};
+
+const checkWinner = () => {
+  for (let pattern of winPatterns) {
+    let pos1Val = boxes[pattern[0]].innerText;
+    let pos2Val = boxes[pattern[1]].innerText;
+    let pos3Val = boxes[pattern[2]].innerText;
+
+    if (pos1Val != "" && pos2Val != "" && pos3Val != "") {
+      if (pos1Val === pos2Val && pos2Val === pos3Val) {
+        showWinner(pos1Val);
+        return true;
+      }
+    }
+  }
+};
 
 newGameBtn.addEventListener("click", resetGame);
-
 resetBtn.addEventListener("click", resetGame);
-
-boxes.forEach((box)=> {
-    box.addEventListener("click", (e) => {
-        if (turn0) {
-            turn0 = false;
-            box.innerHTML = "X";
-        } else {
-            turn0 = true;
-            box.innerHTML = "O";
-        }
-        checkForWinner();
-        box.disabled = true;
-    });
-});
-
-
-
-boxes.addEventListener("click", checkForWinner());
